5f371ba4d88ef2c12b32b5dfcf73b4103e5caebd
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-05-27 17:32:33 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -72,8 +72,8 @@ io.on('connection', (socket) => {
     //When the host connects from the game view
     socket.on('host-join-game', (data) => {
         var oldHostId = data.id;  
-       // var gamepin2 = Math.floor(Math.random()*90000) + 10000; //new pin for game
-        console.log(data+" id encontrado, id generado... "+socket.id);
+        var gamepin2 = Math.floor(Math.random()*90000) + 10000; //new pin for game
+        console.log(data+" id encontrado, id generado... "+gamepin2+" socket id: "+socket.id);
         io.to(socket.id).emit('hola');
         var game = games.getGame(oldHostId);//Gets game with old host id
         if(game){
