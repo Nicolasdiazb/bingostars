d515e0b4092b1654f292fd14052d49afc18bd8a2
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-05-27 18:15:27 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ io.on('connection', (socket) => {
         var oldHostId = data.id;  
         var gamepin2 = Math.floor(Math.random()*90000) + 10000; //new pin for game
         console.log(data+" id encontrado, id generado... "+gamepin2+" socket id: "+socket.id);
-        io.emit('hola');
+        socket.emit('hola', { hello: 'world' });
         io.to(socket.id).emit('conn');
         console.log("enviado");
     });
