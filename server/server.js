d5d6f5fde85118dc53d355976e7df9f46a888118
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-05-28 00:22:29 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -87,9 +87,7 @@ io.on('connection', (socket) => {
     });
     //When player connects for the first time
     socket.on('player-join', (params) => {
-        
-        var obj = JSON.parse(params);
-                console.log('entra '+params +' '+params.name+' +params.pin+' '+obj);
+                console.log('entra '+params.data +' '+params.name+' +params.pin+' '+obj);
         var gameFound = false; //If a game is found with pin provided by player
         
         //For each game in the Games class
