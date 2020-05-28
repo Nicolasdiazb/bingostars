c900c02fd548c0a0f7b1cac1aa9559ab0f22d8b9
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-05-28 14:15:40 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -100,7 +100,7 @@ io.on('connection', (socket) => {
                 var hostId = games.games[i].hostId; //Get the id of host of game
                 
                 
-                players.addPlayer(hostId, socket.id, params.nameID, {score: 0, answer: 0}); //add player to game
+                players.addPlayer(hostId, socket.id, params.nameID); //add player to game
                 
                 socket.join(params.pin); //Player is joining room based on pin
                 
