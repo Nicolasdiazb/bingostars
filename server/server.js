e71ad60162fe779df44096947609561b6c26ff0e
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-06-12 06:55:30 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -217,10 +217,12 @@ io.on('connection', (socket) => {
                                 if(playersInGame[n].onGame ==false)
                                 {
                                     console.log("player "+playersInGame[n].playerId+" i outside");
-                                    //io.to(playersInGame[n].playerId).emit('gameStarted', playersInGame);
+                                    io.to(playersInGame[n].playerId).emit('gameStarted', playersInGame);
                                     //io.to(playersInGame[n].playerId).emit('refreshBallots', games.games[gamePos].activeBallots);//Sending player all ballots 
+                                }else
+                                {
+                                    io.to(playersInGame[n].playerId).emit('newBallot', randNum);//Sending players a ballot 
                                 }
-                                    io.to(playersInGame[n].playerId).emit('newBallot', randNum);//Sending players a ballot  
                                 
                             }
                        }else
