b737c913211857016a4786ff4f7e5ee94aaac5f1
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-07-07 14:15:51 -0500

    Create liveGames2.js

---
+++ server/utils/liveGames2.js
@@ -0,0 +1,32 @@
+class LiveGames {
+    constructor () {
+        this.games = [];
+    }
+    addGame(pin, hostId, boardLenght){
+        var ballots = new Array(boardLenght);
+        var activeBallots = new Array(boardLenght);
+        var active = 1;
+        var intervalIdCB;
+        for (var i = 0; i < boardLenght; i++) 
+        {
+            activeBallots[i] = 0;
+        }
+        var game = {pin, hostId, boardLenght, activeBallots, ballots, intervalIdCB};
+        this.games.push(game);
+        return game;
+    }
+    removeGame(hostId){
+        var game = this.getGame(hostId);
+        
+        if(game){
+            this.games = this.games.filter((game) => game.hostId !== hostId);
+        }
+        return game;
+    }
+    getGame(hostId){
+        return this.games.filter((game) => game.hostId === hostId)[0]
+    }
+}
+
+module.exports = {LiveGames};
+© 2020 GitHub, Inc.
