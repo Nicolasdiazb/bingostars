93ccca14922a7578c6bd1f5ac11165e9f81a386a
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-07-07 14:15:03 -0500

    Create Players2.js

---
+++ server/utils/Players2.js
@@ -0,0 +1,31 @@
+class Players {
+    constructor () {
+        this.players = [];
+    }
+    addPlayer(hostId, playerId, nameId, profilePic){
+        var onGame = new Boolean(false);
+        var player = {hostId, playerId, nameId, profilePic, onGame};
+        this.players.push(player);
+        return player;
+    }
+    removePlayer(playerId){
+        var player = this.getPlayer(playerId);
+        
+        if(player){
+            this.players = this.players.filter((player) => player.playerId !== playerId);
+        }
+        return player;
+    }
+    getPlayer(playerId){
+        return this.players.filter((player) => player.playerId === playerId)[0]
+    }
+    updatePlayerId(newPlayerId, _playerId){
+        var playerToChange = this.players.filter((player) => player.playerId === _playerId)[0];
+        playerToChange.playerId = newPlayerId; 
+    }
+    getPlayers(hostId){
+        return this.players.filter((player) => player.hostId === hostId);
+    }
+}
+
+module.exports = {Players};
