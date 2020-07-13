4fece3adb3a1eca40b933753e56d7e4bd4c36deb
Nicolasdiazb <42294457+Nicolasdiazb@users.noreply.github.com>
2020-07-13 11:46:46 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -194,7 +194,7 @@ io.on('connection', (socket) => {
         if(gameFound){
                         
             // Will execute myCallback every 5 seconds 
-            var intervalID = setInterval(SetBallot, 9000);
+            var intervalID = setInterval(SetBallot, 7500);
             games.games[gamePos].intervalIdCB = intervalID;
             
             function SetBallot() {
